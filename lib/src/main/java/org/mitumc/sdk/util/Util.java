794e0ecbebd84788b6b4cd54854af7064c75aa1f
wyuinche <wyuinche.mido@gmail.com>
2021-09-08 17:06:19 +0900

    Fix Util; hexStringToBytes

--- lib/src/main/java/org/mitumc/sdk/util/Util.java
+++ lib/src/main/java/org/mitumc/sdk/util/Util.java
@@ -85,7 +85,7 @@ public class Util {
     }
 
     public static byte[] hexStringToBytes(String s) {
-        int len = s.length() / 2;
+        int len = s.length();
         byte[] data = new byte[len];
         for (int i = 0; i < len; i += 2) {
             data[i / 2] = (byte) ((Character.digit(s.charAt(i), 16) << 4) + Character.digit(s.charAt(i + 1), 16));
