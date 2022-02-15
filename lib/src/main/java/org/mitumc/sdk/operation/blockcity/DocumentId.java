2954c40e35b3c205aa6990591937abcfe65fe992
wyuinche <wyuinche.mido@gmail.com>
2022-02-15 11:37:38 +0900

    Patch; string document id now supported, fix wrong user document toBytes

--- lib/src/main/java/org/mitumc/sdk/operation/blockcity/DocumentId.java
+++ lib/src/main/java/org/mitumc/sdk/operation/blockcity/DocumentId.java
@@ -3,28 +3,27 @@ package org.mitumc.sdk.operation.blockcity;
 import java.util.HashMap;
 
 import org.mitumc.sdk.interfaces.BytesChangeable;
-import org.mitumc.sdk.util.BigInt;
 import org.mitumc.sdk.util.RegExp;
 import org.mitumc.sdk.util.Util;
 
 public class DocumentId implements BytesChangeable {
-    private BigInt id;
+    private String id;
     private String type;
 
     DocumentId(String documentId) {
         RegExp.assertBlockCityDocumentId(documentId);
 
         HashMap<String, String> parsed = Util.parseDocumentId(documentId);
-        this.id = new BigInt(parsed.get("id"));
+        this.id = parsed.get("id");
         this.type = parsed.get("suffix");
     }
 
     public String getDocumentId() {
-        return this.id.getValue() + this.type;
+        return this.id + this.type;
     }
 
     public String getId() {
-        return this.id.getValue();
+        return this.id;
     }
 
     public String getType() {
